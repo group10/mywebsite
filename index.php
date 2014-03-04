74153887557c2b10be88e75f68ee71fc711a42ce
TeacherLight <abcxyz7979@yahoo.com.tw>
2014-03-04 11:22:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>國立臺北教育大學_資訊教育學習網</title>
+<title>國立臺北教育大學_資訊教育學習網</title> 我是陳光禧
 </head>
 
 <body>
